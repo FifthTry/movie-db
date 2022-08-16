c9f42f194427d106c3edae4f22a90de633d60482
abrark <abrark.asahi@gmail.com>
2022-08-16 19:00:34 +0530

    wip

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -6,3 +6,11 @@ import django.http
 def movie_list(req: django.http.HttpRequest):
     d = {"name": "Aditi Rai"}
     return django.http.JsonResponse(d, status=200)
+
+
+# Movie list should come from Database
+
+# There will be an extra parameter in query `domain` based on filter the movies
+# for a user
+
+# Optional Query. Sort: [release-date, rating, updated_on]
