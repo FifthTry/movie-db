819331232e13b00b4b12294e978bc3da7a776dfe
abrark <abrark.asahi@gmail.com>
2022-08-16 18:56:39 +0530

    wip

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+import django.http
 
 # Create your views here.
+
+
+def movie_list(req: django.http.HttpRequest):
+    d = {"name": "Aditi Rai"}
+    return django.http.JsonResponse(d, status=200)
