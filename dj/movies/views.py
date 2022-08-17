6ef43e44bd1f6f978c5051d5a1eeaa4bef53ceb1
ADITIRAI290698 <aditi@fifthtry.com>
2022-08-17 13:24:00 +0530

    update

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -4,9 +4,15 @@ import django.http
 
 
 def movie_list(req: django.http.HttpRequest):
-    d = {"name": "Aditi Rai"}
-    return django.http.JsonResponse(d, status=200)
+    record_response = {"page-number":1,"next":"next", "previous":"previous", "count":5,"list movies":"bj"}
 
+    record_movie = {"id": 1, "title": "Laal Singh Chaddha","release-date": "11 August 2022", "poster": "Laal Singh Chaddha Movies",
+         "director": "Advait Chandan", "description": "LAAL SINGH CHADDHA, a simple man whose extraordinary journey will fill you with love, warmth & happiness.", "rating" : 6, "review-count": 2, "list reviews": "bj"  }
+
+
+    record_review = {"id": 1, "title": "Laal Singh Chaddha", "description": "Very cheap version of the Original movie. Very slow screenplay.Unnecessary things are forced to the movie. Lack of logic, lack of emotions.","reviewer": "imDb", "rating": 5}
+
+    return django.http.JsonResponse([record_response,record_movie,record_review], status=200)
 
 # Movie list should come from Database
 
