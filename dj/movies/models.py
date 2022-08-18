03f748617da7361a1744a414282b89bb47c28067
abrark <abrark.asahi@gmail.com>
2022-08-18 12:11:09 +0530

    models

--- dj/movies/models.py
+++ dj/movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=1024)
+    release_date = models.DateField()
+    poster = models.CharField(max_length=1024)
+    director = models.CharField(max_length=1024)
+    description = models.TextField(null=True, blank=True)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=1024)
+    description = models.TextField()
+    reviewer = models.CharField(max_length=1024)
+    # This null because user may not give rating
+    rating = models.IntegerField(null=True)
