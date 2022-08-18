2bfc07888eb390d071a302f65f02ff84057e421c
abrark <abrark.asahi@gmail.com>
2022-08-18 11:54:17 +0530

    pyfmt

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -4,15 +4,38 @@ import django.http
 
 
 def movie_list(req: django.http.HttpRequest):
-    record_response = {"page-number":1,"next":"next", "previous":"previous", "count":5,"list movies":"bj"}
+    record_response = {
+        "page-number": 1,
+        "next": "next",
+        "previous": "previous",
+        "count": 5,
+        "list movies": "bj",
+    }
 
-    record_movie = {"id": 1, "title": "Laal Singh Chaddha","release-date": "11 August 2022", "poster": "Laal Singh Chaddha Movies",
-         "director": "Advait Chandan", "description": "LAAL SINGH CHADDHA, a simple man whose extraordinary journey will fill you with love, warmth & happiness.", "rating" : 6, "review-count": 2, "list reviews": "bj"  }
+    record_movie = {
+        "id": 1,
+        "title": "Laal Singh Chaddha",
+        "release-date": "11 August 2022",
+        "poster": "Laal Singh Chaddha Movies",
+        "director": "Advait Chandan",
+        "description": "LAAL SINGH CHADDHA, a simple man whose extraordinary journey will fill you with love, warmth & happiness.",
+        "rating": 6,
+        "review-count": 2,
+        "list reviews": "bj",
+    }
 
+    record_review = {
+        "id": 1,
+        "title": "Laal Singh Chaddha",
+        "description": "Very cheap version of the Original movie. Very slow screenplay.Unnecessary things are forced to the movie. Lack of logic, lack of emotions.",
+        "reviewer": "imDb",
+        "rating": 5,
+    }
 
-    record_review = {"id": 1, "title": "Laal Singh Chaddha", "description": "Very cheap version of the Original movie. Very slow screenplay.Unnecessary things are forced to the movie. Lack of logic, lack of emotions.","reviewer": "imDb", "rating": 5}
+    return django.http.JsonResponse(
+        [record_response, record_movie, record_review], status=200
+    )
 
-    return django.http.JsonResponse([record_response,record_movie,record_review], status=200)
 
 # Movie list should come from Database
 
--- dj/proj/urls.py
+++ dj/proj/urls.py
@@ -17,7 +17,4 @@ from django.contrib import admin
 from django.urls import path
 from movies import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("movies/", views.movie_list)
-]
+urlpatterns = [path("admin/", admin.site.urls), path("movies/", views.movie_list)]
