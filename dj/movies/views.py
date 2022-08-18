ac611633d4513070478440f35d1892e81ffc58f4
abrark <abrark.asahi@gmail.com>
2022-08-18 13:08:09 +0530

    add movie and add review

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -3,12 +3,14 @@ import django.http
 # Create your views here.
 
 
-def movie_list(req: django.http.HttpRequest):
+def list_movie(req: django.http.HttpRequest):
     # Request
 
     """
     optional page_number: default 0
     optional count: default 5
+    order by: release_date, rating, updated_on
+    optional domain: <domain is for uniquely identify user, constant unique token>
     """
 
     # Response
@@ -19,11 +21,45 @@ def movie_list(req: django.http.HttpRequest):
     previous: /movie/?p_no=<page_number-1>&count=<count>
     next: /movie/?p_no=<page_number>&count=<count>
     movies
-        get the list of movies by request filter
+        get the list of movies by request filters
+        default order by: release_date
     """
 
     d = {"name": "Aditi Rai"}
     return django.http.JsonResponse(d, status=200)
+
+
+def add_movie(req: django.http.HttpRequest):
+
+    # Request
+    """
+    get from request body
+    title, release_date, poster, director, description, rating, review_count
+    """
+
+    """
+    Create a row in Movie model
+    """
+
+    return django.http.JsonResponse("{redirect to movie page}", status=200)
+
+
+def add_review(req: django.http.HttpRequest):
+    # Request
+    """
+    movie_id:
+    title:
+    optional description:
+    reviewer: token
+    optional rating
+    """
+
+    """
+    Check if movie_id present or not
+    Create a row in Review Model
+    """
+
+    return django.http.JsonResponse("redirect to movie page", status=200)
 
 
 # Movie list should come from Database
