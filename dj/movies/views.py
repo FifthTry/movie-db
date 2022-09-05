424f7476f87de85ba514cd7efaa4d1406df05aef
Aviral Verma <106665143+AviralVerma13@users.noreply.github.com>
2022-09-05 08:22:29 -0700

    Added moviepage url

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -1,141 +1,26 @@
-import django.http
-from .models import Movie, Review
-import json
-from django.core import serializers
-from django.views.decorators.csrf import csrf_exempt
+"""proj URL Configuration
 
-# Create your views here.
-
-
-def list_movie(req: django.http.HttpRequest):
-    # Request
-
-    """
-    optional page_number: default 0
-    optional count: default 5
-    order by: release_date, rating, updated_on
-    optional domain: <domain is for uniquely identify user, constant unique token>
-    """
-    page_number = req.GET.get("p_no", 1)
-    items = req.GET.get("items", 10)
-    """
-    Pagination Logic
-    E.g.: 10 items at every page
-    1: 1 10
-    2: 11 20
-    3: 21 30
-    
-    st = (pno - 1) * items + 1 to
-    end = pno * items
-    """
-
-    order_by = req.GET.get("p_no", 0)
-    movies = Movie.objects.all()[(page_number - 1) * items + 1 : page_number * items]
-
-    return django.http.JsonResponse(
-        {
-            "p_no": page_number,
-            # TODO: Next And Previous both are optional
-            "next": "/movies/p_no=1&items=10",
-            "previous": "/movies/p_no=1&items=10",
-            "items": items,
-            "movies": json.loads(serializers.serialize("json", movies)),
-        },
-        status=200,
-    )
-
-
-@csrf_exempt
-def add_movie(req: django.http.HttpRequest):
-
-    if req.method == "GET":
-        return django.http.HttpResponse("Wrong Method GET", status=405)
-
-    body = json.loads(req.body.decode("utf-8"))
-    movie = Movie.objects.create(
-        title=body["title"],
-        release_date=body["release_date"],
-        poster=body["poster"],
-        director=body["director"],
-        description=body.get("description"),
-    )
-
-    print(movie)
-    # TODO: redirect to movie page
-    return django.http.JsonResponse({"movie": movie.id}, status=200)
-
-@csrf_exempt
-def get_movie(req: django.http.HttpRequest):
-
-    movie = req.GET.get("title")
-
-    return django.http.JsonResponse(
-        {
-            "title": movie.title,
-            "release_date": movie.release_date,
-            "poster": movie.poster,
-            "director": movie.director,
-            "description": movie.description,
-        },
-        status=200,
-    )
-
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-curl -X POST http://127.0.0.1:8001/add-movie/ \
---data-raw '{
-    "title": "Movie Title", 
-    "release_date": "2022-12-10", 
-    "poster": "M Poster", 
-    "director": "Movie Director"
-}'
-"""
+from django.contrib import admin
+from django.urls import path
+from movies import views
 
-
-@csrf_exempt
-def add_review(req: django.http.HttpRequest):
-    # Request
-    """
-    movie_id, title, optional description, reviewer: token, optional rating
-    """
-    if req.method == "GET":
-        return django.http.HttpResponse("Wrong Method GET", status=405)
-
-    body = json.loads(req.body.decode("utf-8"))
-    try:
-        movie = Movie.objects.get(id=body["movie"])
-    except Exception as e:
-        print(e)
-        # TODO: Redirect to error page with 404 error
-        return django.http.HttpResponse("redirect to movie page", status=404)
-
-    review = Review.objects.create(
-        movie=movie,
-        title=body["title"],
-        description=body.get("description"),
-        reviewer=body["reviewer"],
-        rating=body["rating"],
-    )
-    # TODO: redirect to movie page
-    return django.http.JsonResponse(
-        {"review": review.id, "movie": movie.id}, status=200
-    )
-
-
-"""
-curl -X POST http://127.0.0.1:8001/add-review/ \
---data-raw '{
-    "movie": 1, 
-    "title": "Review Title", 
-    "description": "Review Description", 
-    "reviewer": "Movie Reviewer",
-    "rating": 8
-}'
-"""
-
-# Movie list should come from Database
-
-# There will be an extra parameter in query `domain` based on filter the movies
-# for a user
-
-# Optional Query. Sort: [release-date, rating, updated_on]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("movies/", views.list_movie),
+    path("add-movie/", views.add_movie),
+    path("add-review/", views.add_review),
+    path("moviepage/", views.get_movie),
+]
