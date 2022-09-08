8ef59f5a874e03c56b71c034f826f05753b1b7b3
abrark <abrark.asahi@gmail.com>
2022-09-08 11:39:28 +0530

    update

--- dj/movies/views.py
+++ dj/movies/views.py
@@ -64,6 +64,18 @@ def add_movie(req: django.http.HttpRequest):
     # TODO: redirect to movie page
     return django.http.JsonResponse({"movie": movie.id}, status=200)
 
+
+"""
+curl -X POST http://127.0.0.1:8001/add-movie/ \
+--data-raw '{
+    "title": "Movie Title", 
+    "release_date": "2022-12-10", 
+    "poster": "M Poster", 
+    "director": "Movie Director"
+}'
+"""
+
+
 @csrf_exempt
 def get_movie(req: django.http.HttpRequest):
 
@@ -79,17 +91,6 @@ def get_movie(req: django.http.HttpRequest):
         },
         status=200,
     )
-
-
-"""
-curl -X POST http://127.0.0.1:8001/add-movie/ \
---data-raw '{
-    "title": "Movie Title", 
-    "release_date": "2022-12-10", 
-    "poster": "M Poster", 
-    "director": "Movie Director"
-}'
-"""
 
 
 @csrf_exempt
